a2ac9aee646184998aa9ec742b8c4704acba83f6
Lukas Kohl <lukas.kohl@stud.h-da.de>
2021-03-24 12:58:42 +0100

    first ui element(not reacting to action)

--- JPSvisUE/Source/JPSvisUE/Building.cpp
+++ JPSvisUE/Source/JPSvisUE/Building.cpp
@@ -15,21 +15,43 @@ void ABuilding::BeginPlay()
 {
 	Super::BeginPlay();
 
-	vector<Line>* lines = new vector<Line>();
-	lines->resize(7);
-	lines->at(0) = Line(FVector(-10, -10, 0.f), FVector(-10, 70, 0.f));
-	lines->at(1) = Line(FVector(-10, 70, 0.f), FVector(30, 70, 0.f));
-	lines->at(2) = Line(FVector(30, 70, 0.f), FVector(30, 50, 0.f));
-	lines->at(3) = Line(FVector(30, 50, 0.f), FVector(60, 50, 0.f));
-	lines->at(4) = Line(FVector(60, 50, 0.f), FVector(60, -10, 0.f));
-	lines->at(5) = Line(FVector(60, -10, 0.f), FVector(-10, -10, 0.f));
-	lines->at(6) = Line(FVector(30, -10, 0.f), FVector(30, 20, 0.f));
+	float h1 = 20;
+	float h2 = 40;
 
-	floors = SpawnItems<AFloorStructure*>(1, floorStructureClass);
-	for (AFloorStructure* floor : *floors)
-	{
-		floor->Init(lines);
-	}
+	vector<Line>* lines1 = new vector<Line>();
+	lines1->resize(7);
+	lines1->at(0) = Line(FVector(-10, -10, 0.f), FVector(-10, 70, 0.f));
+	lines1->at(1) = Line(FVector(-10, 70, 0.f), FVector(30, 70, 0.f));
+	lines1->at(2) = Line(FVector(30, 70, 0.f), FVector(30, 50, 0.f));
+	lines1->at(3) = Line(FVector(30, 50, 0.f), FVector(60, 50, 0.f));
+	lines1->at(4) = Line(FVector(60, 50, 0.f), FVector(60, -10, 0.f));
+	lines1->at(5) = Line(FVector(60, -10, 0.f), FVector(-10, -10, 0.f));
+	lines1->at(6) = Line(FVector(30, -10, 0.f), FVector(30, 20, 0.f));
+
+	vector<Line>* lines2 = new vector<Line>();
+	lines2->resize(7);
+	lines2->at(0) = Line(FVector(-10, -10, h1), FVector(-10, 70, h1));
+	lines2->at(1) = Line(FVector(-10, 70, h1), FVector(30, 70, h1));
+	lines2->at(2) = Line(FVector(30, 70, h1), FVector(30, 50, h1));
+	lines2->at(3) = Line(FVector(30, 50, h1), FVector(60, 50, h1));
+	lines2->at(4) = Line(FVector(60, 50, h1), FVector(60, -10, h1));
+	lines2->at(5) = Line(FVector(60, -10, h1), FVector(-10, -10, h1));
+	lines2->at(6) = Line(FVector(30, -10, h1), FVector(30, 20, h1));
+
+	vector<Line>* lines3 = new vector<Line>();
+	lines3->resize(7);
+	lines3->at(0) = Line(FVector(-10, -10, h2), FVector(-10, 70, h2));
+	lines3->at(1) = Line(FVector(-10, 70, h2), FVector(30, 70, h2));
+	lines3->at(2) = Line(FVector(30, 70, h2), FVector(30, 50, h2));
+	lines3->at(3) = Line(FVector(30, 50, h2), FVector(60, 50, h2));
+	lines3->at(4) = Line(FVector(60, 50, h2), FVector(60, -10, h2));
+	lines3->at(5) = Line(FVector(60, -10, h2), FVector(-10, -10, h2));
+	lines3->at(6) = Line(FVector(30, -10, h2), FVector(30, 20, h2));
+
+	floors = SpawnItems<AFloorStructure*>(3, floorStructureClass);
+	floors->at(0)->Init(lines1);
+	floors->at(1)->Init(lines2);
+	floors->at(2)->Init(lines3);
 }
 
 // Called every frame
