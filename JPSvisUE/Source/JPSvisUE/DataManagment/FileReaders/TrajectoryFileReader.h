b733ea5be1582e52d87d78efb7bf2a43b6279863
Lukas Kohl <lukas.kohl@stud.h-da.de>
2021-04-15 13:05:33 +0200

    extendet TrajectoryFileReader to enable use of different File Types

--- JPSvisUE/Source/JPSvisUE/DataManagment/FileReaders/TrajectoryFileReader.h
+++ JPSvisUE/Source/JPSvisUE/DataManagment/FileReaders/TrajectoryFileReader.h
@@ -7,8 +7,11 @@
 #include <string>
 #include <fstream>
 #include <stdexcept>
+#include <vector>
 
 class CacheLine;
+class CacheEntry;
+enum DataType { BIN, TXT, NONE };
 /**
  * 
  */
@@ -19,4 +22,11 @@ public:
 	~TrajectoryFileReader();
 	static CacheLine LoadCacheLine(int startAddress, int count, std::string filePath,int tag, unsigned int lruID);
 	static int GetFrames(std::string filePath);
+private:
+	static CacheLine CacheLineConstractor(std::vector<CacheEntry> entries,int tag, unsigned int lruID);
+	static std::vector<CacheEntry> LoadCacheLineBin(int startAddress, int count, std::string filePath);
+	static std::vector<CacheEntry> LoadCacheLineTxt(int startAddress, int count, std::string filePath);
+	static int GetFramesBin(std::string filePath);
+	static int GetFramesTxt(std::string filePath);
+	static DataType GetDataType(std::string filePath);
 };
